e15334a24b39dacc7d09ac9075872847f053a191
Namsik-Yoon <skatlr0101@gmail.com>
2021-02-24 01:14:17 +0000

    fix yaml and test.py

--- test.py
+++ test.py
@@ -4,6 +4,7 @@ from sklearn.metrics import mean_squared_error as mse
 import json
 
 model = pickle.load(open("models/model.pkl", "rb"))
+model_name = open("model.txt", 'r').read()
 
 # Generate some data for validation
 X_test, y = make_regression(1000,n_features = 10)
@@ -14,7 +15,7 @@ y_hat = model.predict(X_test)
 # squared=Fase -> False returns RMSE value
 metric = mse(y, y_hat, squared=False)
 
-with open("metrics.txt", 'w') as outfile:
-    outfile.write(f"Test RMSE : {metric:.2f}")
+with open("metrics.json", 'w') as outfile:
+    json.dump({ "model_name": model_name, "RMSE": rmse}, outfile)
 
 
